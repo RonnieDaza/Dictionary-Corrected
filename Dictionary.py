3aadf39964c8d3e3b8a535edcbdd168025ec58b6
RonnieDaza <daza.ronnie.09212002@gmail.com>
2022-11-24 09:44:48 +0800

    Add menu

---
+++ Dictionary.py
@@ -0,0 +1,38 @@
+print("-----------------------------")
+print("MENU:")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+print("-----------------------------")
+
+dict_info =  {"Mark": ["19", "Manila", "09123456789"]}
+
+while True:
+
+    choice = int(input("What do you want to do? (1-3) "))
+
+    if choice == 1:
+        fullName = input("Name: ")
+        age = int(input("Age: "))
+        address = input("Address: ")
+        country = input("Country: ")
+        phoneNumber = input("Phone Number: ")
+        gender = input("Gender: ")
+        email = input("Email: ")
+        userInfo = dict_info[fullName] = {"age": age,
+                             "address": address,
+                             "country": country,
+                             "phone number": phoneNumber,
+                             "gender": gender,
+                             "email": email,
+                             }
+        userList = dict_info.update(userInfo)
+        print("Saved!")
+    
+    elif choice == 2:
+        
+    
+    elif choice == 3:
+        exit = input("Are you sure? (Yes/No) ")
+        if exit == "Yes":
+            break
